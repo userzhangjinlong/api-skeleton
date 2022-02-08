0ae993454b17e974508fbaeb1bc8770854b23b13
张金龙 <zhangjinl@weipaitang.com>
2022-02-08 15:47:02 +0800

    测试日志写入

--- app/Http/Controller/Api/IndexController.go
+++ app/Http/Controller/Api/IndexController.go
@@ -2,6 +2,7 @@ package Api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 )
 
 type Index struct {
@@ -9,6 +10,10 @@ type Index struct {
 
 //Index 首页入口
 func (i *Index) Index(ctx *gin.Context) {
+	logrus.WithFields(logrus.Fields{
+		"code": 200,
+		"data": "success",
+	}).Info("测试日志写入")
 	ctx.JSON(200, gin.H{
 		"code": 200,
 		"msg":  "success",
